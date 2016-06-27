bd17b97e2916d1f4a32aa037e4321b09659dcaf4
katiaVersace <katia.versace@hotmail.it>
2016-06-27 16:11:55 +0200

    ConstraintsPanel is added

---
+++ 27_GiugnoEditor_src/editor/TracksReader.java
@@ -0,0 +1,220 @@
+package editor;
+
+
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TracksReader {
+	
+	private static boolean datatype = false; //false = BE true = LE
+	private static int offset = 0;
+	private static int N = 0;
+	private static BufferedInputStream inp = null;
+	private static float[][] data;
+	private static float[][][] tracks;
+
+	public static void read(String file){
+		
+//		long startTime = System.nanoTime();
+
+		
+//		String path = "C:/Users/Leviathan/Desktop/Experiments/JHU_tracts_on_IIT3_FA_volumes/volume2_fibers.tck";
+		
+		readHeader(file);
+
+		readData();
+        
+//		System.out.println("K SIZE: ");
+//		System.out.println(k.size());
+
+//		float[][] a_track = tracks.get(tracks.size()-1);
+		
+		printTrack(tracks[tracks.length-1]);
+		
+//		long endTime = System.nanoTime();
+//
+//		long duration = endTime - startTime;
+//		
+//		System.out.println(duration);
+
+	}
+	
+	public static void readHeader(String file){
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line = null;
+			while(!(line = br.readLine()).equals("END")){
+				String[] field = line.split(":");
+				if(field[0].equals("datatype") && field[1].contains("LE"))
+					datatype = true;
+				
+				else if(field[0].equals("file")){
+					String f = field[1].substring(field[1].lastIndexOf(' ')+1,field[1].length());
+					offset = Integer.parseInt(f);
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			inp = new BufferedInputStream(new FileInputStream(file));
+			//505(esempio) � il valore di offset da skippare per iniziare a leggere le fibre. Si trova scritto nell'header
+			inp.skip(offset); 
+			// available is the input stream to be read. inp.available/4 because of 4 byte and /3 because of 3D fibers, so /12 
+			N = Math.round(inp.available()/12); 
+			
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+	
+	public static void readData(){
+		
+		data = new float[N][3];
+		List<Integer> k = new ArrayList<Integer>();
+		
+		byte[] buffer = new byte[4];
+//		ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+		try {
+			for(int i = 0; i < N; i++){
+				for(int j = 0; j < 3; j++){
+//					if(inp.available() > 0){ //disabled due to performance issue
+						inp.read(buffer);
+						int nextInt = 0; 
+						if(datatype)
+							nextInt = (buffer[0] & 0xFF) | (buffer[1] & 0xFF) << 8 | (buffer[2] & 0xFF) << 16 | (buffer[3] & 0xFF) << 24;
+						else
+							nextInt = (buffer[0] & 0xFF) | (buffer[1] & 0xFF) << 24 | (buffer[2] & 0xFF) << 16 | (buffer[3] & 0xFF) << 8;
+
+						float nextFloat = Float.intBitsToFloat(nextInt);
+//						System.out.println(nextFloat);
+						data[i][j] = nextFloat; //contains also NAN values
+						if(Float.isNaN(nextFloat) && j == 0) // the other js are NaN too and will be put as a row: NaN NaN NaN
+							k.add(i);
+						
+						//another way to do it
+//						byteBuffer = ByteBuffer.wrap(buffer);
+//						data[i][j] = byteBuffer.order(ByteOrder.LITTLE_ENDIAN).getFloat(); //contains also NAN values
+					}
+//					else{
+//						System.out.println("EOF");
+//						break;
+//					}
+//				}
+			}
+//			System.out.println("END");
+//			if(inp.available() > 0){
+//				System.out.println("THERE'S MORE!");
+//			}
+//			else{
+//				System.out.println("IT'S REALLY THE EOF!");
+//			}
+
+			inp.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		formatTracks(k);
+		
+	}
+	
+	public static void formatTracks(List<Integer> k){
+		
+		int pk = 0;
+		
+//		List<float[][]> tracks = new ArrayList<float[][]>();
+		tracks = new float[k.size()][][];
+		
+		for(int i = 0; i < k.size(); i++){
+			int it = 0;
+//			System.out.println(k.get(i)-pk);
+			
+//			float[][] track = new float[k.get(i)-pk][3];
+			tracks[i] = new float[k.get(i)-pk][3];
+			while(pk < k.get(i)){
+				for(int j = 0; j < 3; j++){
+					tracks[i][it][j] = data[pk][j];
+				}
+				it++;
+				pk++;
+			}
+			pk++;
+		}
+	}
+	
+	public static void printTrack(float[][] a_track){
+		
+		for(int i = 0; i < a_track.length; i++){
+			for(int j = 0; j < 3; j++) {
+				System.out.print(a_track[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static boolean isDatatype() {
+		return datatype;
+	}
+
+	public static void setDatatype(boolean datatype) {
+		TracksReader.datatype = datatype;
+	}
+
+	public static int getOffset() {
+		return offset;
+	}
+
+	public static void setOffset(int offset) {
+		TracksReader.offset = offset;
+	}
+
+	public static int getN() {
+		return N;
+	}
+
+	public static void setN(int n) {
+		N = n;
+	}
+
+	public static BufferedInputStream getInp() {
+		return inp;
+	}
+
+	public static void setInp(BufferedInputStream inp) {
+		TracksReader.inp = inp;
+	}
+
+	public static float[][] getData() {
+		return data;
+	}
+
+	public static void setData(float[][] data) {
+		TracksReader.data = data;
+	}
+
+	public static float[][][] getTracks() {
+		return tracks;
+	}
+
+	public static void setTracks(float[][][] tracks) {
+		TracksReader.tracks = tracks;
+	}
+	
+	
+}
---
+++ 27_GiugnoEditor_src/editor/VincoliPanel.java
@@ -0,0 +1,270 @@
+package editor;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
+
+
+public class VincoliPanel extends JPanel {
+	PaintArea p;
+	
+	public VincoliPanel(PaintArea p){
+		this.p=p;
+		
+		
+		setSize(100, 100);
+		setLayout(null);
+		
+		DefaultTableModel t = new DefaultTableModel();
+		MyTable TabellaPrenotati = new MyTable();
+		TabellaPrenotati(TabellaPrenotati, t);
+		
+		
+		JLabel lblVincoli = new JLabel("VINCOLI DI SIMILARITA'");
+		lblVincoli .setForeground(Color.BLACK);
+		lblVincoli .setFont(new Font("Kokonor", Font.PLAIN, 30));
+		lblVincoli .setBounds(80, 10, 472, 80);
+		add(lblVincoli );
+		
+		JButton addRowButton = new JButton("+");
+		addRowButton.setLocation(30,100);
+		addRowButton.setSize(new Dimension(50, 50));
+		addRowButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+addRow(t, TabellaPrenotati);
+			
+
+			}
+		});
+		
+		addRowButton.setToolTipText("Add constraint");
+		add(addRowButton);
+		
+		
+		JButton removeRowButton = new JButton("-");
+		removeRowButton.setLocation(30,160);
+		removeRowButton.setSize(new Dimension(50, 50));
+		removeRowButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+removeSelectedRow(TabellaPrenotati,t);
+			
+
+			}
+		});
+		
+		removeRowButton.setToolTipText("Remove constraint");
+		add(removeRowButton);
+		
+		
+		
+		JButton loadButton = new JButton("Load");
+		loadButton.setLocation(100,400);
+		loadButton.setSize(new Dimension(100, 50));
+		loadButton.setToolTipText("Load constraint file");
+		loadButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+
+				 JFileChooser chooser = new JFileChooser();
+				    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				        "txt", "txt");
+				    chooser.setFileFilter(filter);
+			int returnVal=	     chooser.showOpenDialog(null);   
+				    if(returnVal == JFileChooser.APPROVE_OPTION) {
+//				       System.out.println("You chose to open this file: " +
+//				            chooser.getSelectedFile().getPath());
+				   
+				    
+				    
+				    BufferedReader in = null;
+try {
+    in = new BufferedReader(new FileReader( chooser.getSelectedFile().getPath()));
+    while (t.getRowCount() > 0) {
+        t.removeRow(0);
+     } 
+    
+    String read = null;
+       while ((read = in.readLine()) != null) {
+        String[] splited = read.split("\\s+");
+        try {if(splited.length==2 && Integer.parseInt(splited[0])>=0 && Integer.parseInt(splited[0])<10 && Integer.parseInt(splited[1])>=0 && Integer.parseInt(splited[1])<10)
+        	addRowFromFile(TabellaPrenotati,t,splited);
+        else{JOptionPane.showMessageDialog(null,
+          		"File non valido, verificare il contenuto");
+          while (t.getRowCount() > 0) {
+             t.removeRow(0);
+          }
+          break;}}
+
+          catch (NumberFormatException exc) {JOptionPane.showMessageDialog(null,
+          		"File non valido, verificare il contenuto");
+          while (t.getRowCount() > 0) {
+             t.removeRow(0);
+          }
+          break;}
+      
+        
+        
+        
+      
+    }
+} catch (IOException e1) {
+    System.out.println("There was a problem: " + e1);
+    e1.printStackTrace();
+} finally {
+    try {
+        in.close();
+    } catch (Exception e1) {
+    }
+}
+
+
+				    }}
+		});
+		add(loadButton);
+		
+		JButton generateButton = new JButton("Generate");
+		generateButton.setLocation(250,400);
+		generateButton.setSize(new Dimension(150, 50));
+		generateButton.setToolTipText("Get result");
+		generateButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+p.getLineColorTransformation();
+
+			}
+		});
+		add(generateButton);
+		
+		
+		
+		
+		}
+	
+	
+    public void setUpStringColumn(JTable table,
+                                 TableColumn stringColumn) {
+        //Set up the editor for the sport cells.
+        JComboBox comboBox = new JComboBox();
+        comboBox.addItem("0");
+        comboBox.addItem("1");
+        comboBox.addItem("2");
+        comboBox.addItem("3");
+        comboBox.addItem("4");
+        comboBox.addItem("5");
+        comboBox.addItem("6");
+        comboBox.addItem("7");
+        comboBox.addItem("8");
+        comboBox.addItem("9");
+        stringColumn.setCellEditor(new DefaultCellEditor(comboBox));
+
+        //Set up tool tips for the sport cells.
+        DefaultTableCellRenderer renderer =
+                new DefaultTableCellRenderer();
+        renderer.setToolTipText("Click for combo box");
+        stringColumn.setCellRenderer(renderer);
+    }
+	
+	
+public void TabellaPrenotati(MyTable TabellaPrenotati, DefaultTableModel t) 
+{
+	t.addColumn("First String");
+    t.addColumn("Second String");	
+	
+		
+TabellaPrenotati.setModel(t);
+	setLayout(null);
+	
+	JScrollPane ScrollCatalogo = new JScrollPane(TabellaPrenotati);
+	ScrollCatalogo.setBounds(100, 100, 300, 200);
+	add(ScrollCatalogo);
+  
+	}
+	
+public void addRow(DefaultTableModel t1, JTable t){
+	 String[] emptyRow = { "" };
+	 setUpStringColumn(t, t.getColumnModel().getColumn(0)); 
+	 setUpStringColumn(t, t.getColumnModel().getColumn(1)); 
+	 t1.addRow(emptyRow);
+	   
+	  
+}
+
+public void addRowFromFile(JTable t,DefaultTableModel t1,  String [] s){
+	setUpStringColumn(t, t.getColumnModel().getColumn(0)); 
+	 
+	 setUpStringColumn(t, t.getColumnModel().getColumn(1)); 
+	 t1.addRow(s);
+	   
+	  
+}
+
+
+
+public void removeSelectedRow(JTable table,DefaultTableModel t){
+	
+	if(table.getSelectedRow()!=-1)
+		t.removeRow(table.getSelectedRow());
+else JOptionPane.showMessageDialog(null,
+		"Seleziona un vincolo");
+}
+
+
+	class MyTable extends JTable {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private boolean Editable = true;
+
+		@Override
+		public boolean isCellEditable(int row, int column) {
+
+			return Editable;
+		}
+
+		public void setEditable() {
+
+			Editable = true;
+		}
+
+		public void setNoEditable() {
+
+			Editable = false;
+		}
+	
+
+	
+}
+
+}
